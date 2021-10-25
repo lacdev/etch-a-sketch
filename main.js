c1416e9baf341bf35ca8d41a22bfb550b3ea5f60
German <german.mtz@protonmail.com>
2021-10-25 11:47:17 -0500

    fix create grid for loop append sequence

--- main.js
+++ main.js
@@ -12,12 +12,12 @@ function createGrid(size) {
     for (let i = 0; i < size; i++) {
         const row = document.createElement('div')
         row.classList.add('row')
-        container.appendChild(row)
         for (let j = 0; j < size; j++) {
         const cell = document.createElement('div')
         cell.classList.add('cell')
         row.appendChild(cell)
         }
+        container.appendChild(row)
 }
 }
 
