44d8adf922516bb8922315abc2c59e23aa00ef97
German <german.mtz@protonmail.com>
2021-10-25 22:57:13 -0500

    fix width and height on cells, also fix clear and random buttons on new grid size

--- main.js
+++ main.js
@@ -1,32 +1,29 @@
 console.log("etch a sketch project")
 
 const container = document.querySelector(".container")
+const totalSize = container.scrollWidth;
 
-function createGrid(size) {
+const createGrid = (size) => {
     for (let i = 0; i < size * size; i++) {
         const cell = document.createElement('div')
         cell.classList.add('cell')
+        cell.style.minWidth = `${100 / size}%`
+        cell.style.minHeight = `${100 / size}%`
         container.appendChild(cell)
 }
 }
 
+//Initial grid size
 createGrid(16)
 
 const range = document.querySelector('#range')
-
-const cells = container.querySelectorAll('.cell')
-
 const randomColorButton = document.querySelector('.random-button')
-
 const colorPicker = document.querySelector('#color')
-
 const blackColorButton = document.querySelector('.black-button')
-
 const eraser = document.querySelector('#erase')
-
 const clearButton = document.querySelector('.clear-button')
 
-function removeGrid(){
+const removeGrid = () => {
     //while firstElementChild exists, remove it
     while (container.firstChild){
         container.removeChild(container.firstChild)
@@ -35,52 +32,49 @@ function removeGrid(){
 
 range.addEventListener('input', (event) => {
     let size = parseInt(event.target.value)
-    console.log(size)
     removeGrid()
     createGrid(size)
-    // container.textContent = ''
-    // createGrid(size)
 })
 
 const clearGrid = () => {
-    cells.forEach((cell) => {
-        cell.style = 'background-color: white'
+    document.querySelectorAll('.cell').forEach((cell) => {
+        cell.style.backgroundColor = '#FFFFFF'
     })
 }
 
 clearButton.addEventListener('click', clearGrid)
 
 blackColorButton.addEventListener('click', ()=> {
-    cells.forEach((cell) => {
+    document.querySelectorAll('.cell').forEach((cell) => {
         cell.classList.remove('random')    
     })
     colorPicker.value = "#000000"
 })
 
 eraser.addEventListener('click', ()=> {
-    cells.forEach((cell) => {
-    cell.classList.remove('random')    
+    document.querySelectorAll('.cell').forEach((cell) => {
+        cell.classList.remove('random')
     })
     colorPicker.value = '#FFFFFF'
 })
 
 randomColorButton.addEventListener('click', ()=> {
-    cells.forEach((cell) => {
-    cell.classList.toggle('random')
+    document.querySelectorAll('.cell').forEach((cell)=> {
+        cell.classList.toggle('random')
     })
 })
 
 colorPicker.addEventListener('input', ()=> {
-    cells.forEach((cell)=> {
+    document.querySelectorAll('.cell').forEach((cell) => {
         cell.classList.remove('random')
     })
-} )
+})
 
 const getRandomColor = () => {
-    var letters = '0123456789ABCDEF'
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
+    let letters = '0123456789ABCDEF'
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * letters.length)];
     }
     return color;
 }
@@ -90,9 +84,9 @@ const paintCurrentColor = (event) => {
     if (event.target.classList.contains('random')) {
         currentColor = getRandomColor()
     }
-    event.target.style = `background-color: ${currentColor}`
+    event.target.style.backgroundColor = currentColor
 }
 
-container.addEventListener('mouseover', paintCurrentColor )
+container.addEventListener('mouseover', paintCurrentColor)
 
 
